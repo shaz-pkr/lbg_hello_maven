589b12b3359cba29ebeafc85faf66d8af6a78002
Shahz <shahzeb.palekar@yahoo.co.uk>
2026-02-24 14:29:45 +0000

    EDIT:version 2.0

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
          System.out.println(sayHelloToSomeone("Shahz"));
+         System.out.println(sayHelloToSomeone("Sladey"));
         System.out.println(sayGoodbye());
     }
 
